5ff9a07a4f49b9e422620208ce41d77361e0ba11
Peter Samarin <petersamarin@gmail.com>
2016-02-17 22:58:40 +0100

    Add LPM tests

--- tests/all-tests.rkt
+++ tests/all-tests.rkt
@@ -1,12 +1,13 @@
 (require racket racket/main rackunit)
+(require "data.rkt")
 
 ;; TODO: use load for now, change later to "require"
 (load "../atmega163-simulator.rkt")
 
 ;; Helper procedures
-(define (step-and-compare-reg-content test-nr reg val)
-  (fetch-and-decode)
-  (check-equal? (sram-get-byte reg) val test-nr))
+(define (step-and-compare-reg-content test-label reg val (n-instr 1))
+  (for ([i n-instr]) (fetch-and-decode))
+  (check-equal? (sram-get-byte reg) val test-label))
 
 ;; reset the machine, load everything, 
 ;; and let the init code execute
@@ -24,10 +25,9 @@
   (set! PC (lookup-symbol label)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Testing LDI
+;; LDI
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (prepare-machine "startTestLDI")
-(set! debug? #t)
 (for ([i 16])
   (step-and-compare-reg-content
    (string-append "LDI-r16-1-" 
@@ -44,11 +44,38 @@
   (step-and-compare-reg-content
    (string-append "LDI-r" (number->string reg) "-ff-") reg #xff))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; LPM RXX,Z for registers R0-R29
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(for ([reg 30])
+  (prepare-machine (string-append "startTestLPMR" 
+                                  (number->string reg)))
+  (fetch-and-decode)
+  (fetch-and-decode)
+  (for ([i 256])
+    (step-and-compare-reg-content
+     (string-append "LPM-" (number->string reg) 
+                    "-sbox-" (number->string i))
+     reg
+     (vector-ref sbox i))
+    (fetch-and-decode)
+    (fetch-and-decode)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Testing LPM
+;; LPM RXX,Z+ for registers R0-R29
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(prepare-machine "startTestLPM")
-(set! debug? #t)
-(fetch-and-decode)
+(for ([reg 30])
+  (prepare-machine (string-append "startTestLPMZplusR" 
+                                  (number->string reg)))
+  (fetch-and-decode)
+  (fetch-and-decode)
+  (for ([i 256])
+    (step-and-compare-reg-content
+     (string-append "LPM-Z+-" (number->string reg) 
+                    "-sbox-" (number->string i))
+     reg
+     (vector-ref sbox i))
+    (fetch-and-decode)))
+
+
 
