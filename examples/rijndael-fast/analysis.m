5eceb45b406d7a97726395d74c3f2c6d015844ab
Peter Samarin <petersamarin@gmail.com>
2017-02-15 18:29:07 +0100

    Rearrange

--- examples/rijndael-fast/analysis.m
+++ examples/rijndael-fast/analysis.m
@@ -1,12 +1,6 @@
-## Make nice plots
-set(0, 'DefaulttextInterpreter', 'none')
-figure(1)
-papersize = [30,21]*0.3;
-set (gcf, "paperorientation", "landscape") 
-set (gcf, "papersize", papersize)
-set (gcf, "paperposition", [0 0, papersize])
-
+#############################################
 ## Get data from the leakage file
+#############################################
 f       = fopen("leakage-trace.txt");
 nTraces = fread(f, 1, "*uint32","b"); ## "b" - big endian
 keys = zeros(nTraces,16,"uint8");
@@ -33,32 +27,11 @@ cc = cell2mat(cc);
 
 ## Add extra noise, because some instructions have constant leakage,
 ## which will result in zero variance
-leakage = leakage + rand(size(leakage))+0.001;
+leakage = leakage + rand(size(leakage))+0.0001;
 
-plot(cc(1,:),leakage(1,:))
-## plot(cc(1,:),mean(leakage))
-ylabel("leakage")
-xlabel("clock cycle")
-xlim([0 cc(end)])
-print("leakage.png")
-
-## Get the data from the execution trace
-traceFile = "execution-trace.txt";
-[instrCC,instrAddr,instrOPcode,instrName,instrDuration,instrGenericName,instrIDs,instrAnnotation] = textread(traceFile, '%d|%d|%d|%s|%d|%s|%d|%s' ,'delimiter' , '|', 'whitespace', "\n");
-instrName = char(instrName);
-instrCC = double(instrCC);
-instrDuration = double(instrDuration);
-instrIDs = double(instrIDs);
-instrGenericName = char(instrGenericName);
-instrAnnotation = char(instrAnnotation);
-
-plot(instrCC,instrIDs)
-ylabel("instruction ID")
-xlabel("clock cycle")
-xlim([0 instrCC(end)])
-print("instr-id.png")
-
+#############################################
 ## Simple CPA
+#############################################
 ## load sbox and hamming weights tables
 source("sbox-and-hws.m")
 ## allocate
@@ -78,9 +51,42 @@ end
 printf("%d key bytes correct\n", sum(guessed_key == keys(1,:)));
 
 
+## Make nice plots
+set(0, 'DefaulttextInterpreter', 'none')
+figure(1)
+papersize = [30,21]*0.5;
+set (gcf, "paperorientation", "landscape") 
+set (gcf, "papersize", papersize)
+set (gcf, "paperposition", [0 0, papersize])
 
 plot(corrs'+repmat(0:15,nVals,1))
 xlabel("clock cycle")
-ylabel("correlation for each key byte")
+ylabel("correlation for each winning key byte")
+set (gca, 'ytick', 0:16)
 ylim([0 16])
 xlim([0 nVals])
+title("CPA on each key byte")
+print("CPA-each-keybyte.png")
+
+plot(cc(1,:),leakage(1,:))
+## plot(cc(1,:),mean(leakage))
+ylabel("mean leakage")
+xlabel("clock cycle")
+xlim([0 cc(end)])
+print("leakage.png")
+
+## Get the data from the execution trace
+traceFile = "execution-trace.txt";
+[instrCC,instrAddr,instrOPcode,instrName,instrDuration,instrGenericName,instrIDs,instrAnnotation] = textread(traceFile, '%d|%d|%d|%s|%d|%s|%d|%s' ,'delimiter' , '|', 'whitespace', "\n");
+instrName = char(instrName);
+instrCC = double(instrCC);
+instrDuration = double(instrDuration);
+instrIDs = double(instrIDs);
+instrGenericName = char(instrGenericName);
+instrAnnotation = char(instrAnnotation);
+
+plot(instrCC,instrIDs)
+ylabel("instruction ID")
+xlabel("clock cycle")
+xlim([0 instrCC(end)])
+print("instr-id.png")
